12e39c62f20c4d5903e01197d33e647f602b8e2c
Chris Paiano <chris.paiano@nike.com>
2014-12-12 20:23:27 -0800

    -Now the honeypotmonitor.sh command will beep when new log activity is detected each cycle (if the file size of the log has changed; this does not necessarily mean that new credentials have been captured, only that new raw log activity is present)

--- honeypotmonitor.sh
+++ honeypotmonitor.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
+honeyPotLogFile="/var/log/xrdp.log"
+
+function updateHoneyPotLogModifiedTimeAndBeepIfChanged()
+{
+    lastHoneyPotLogModifiedTime=$honeyPotLogModifiedTime
+    honeyPotLogModifiedTime="$(stat --printf="%Z" $honeyPotLogFile)"
+#    if [ -z ${lastHoneyPotLogModifiedTime+x} ];
+#    then
+#        echo lastHoneyPotLogModifiedTime is unset; this is the first run, no beep allowed...
+#    else
+        if [ "$honeyPotLogModifiedTime" != "$lastHoneyPotLogModifiedTime" ];
+        then
+#            echo ========--------********NEW LOGIN CREDENTIALS CAPTURED!********--------========
+            echo NEW LOG ACTIVITY CAPTURED!!!!
+            echo -ne '\a'
+        fi
+#    fi
+}
 
 while [  1 -lt 2 ]; do
-    echo "Polling honeypot at $(date)..."
-    sudo cat /var/log/xrdp.log | grep USER:
+    updateHoneyPotLogModifiedTimeAndBeepIfChanged
+    echo "$(ls -l $honeyPotLogFile)"
+    echo "Polling honeypot logfile $honeyPotLogFile which was last modified at $honeyPotLogModifiedTime..."
+    sudo cat $honeyPotLogFile | grep USER:
     echo "$(date) <--- this is now"
     pitemp.sh
     sleep 30
